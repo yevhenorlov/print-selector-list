bccb291c67c63a5a1d66c8e711e6cf1ea6371cd0
yevhen orlov <yevhenorlov@gmail.com>
2018-07-07 19:33:46 +0300

    prettier

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 import logToFile from 'log-to-file'
 
 function getComponentClassSelectorList(selector) {
-  if (typeof selector !== 'string') throw new TypeError('Attribute must be a string');
+  if (typeof selector !== 'string') {
+    throw new TypeError('Attribute must be a string')
+  }
   const component = document.querySelector(selector)
-  const descendants = Array.prototype.slice.call(component.querySelectorAll("*"), 0);
+  const descendants = Array.prototype.slice.call(
+    component.querySelectorAll('*'),
+    0
+  )
   const classSelectorList = descendants
     .map(el => {
       if (!el.classList.length) return
       return `.${el.classList}`
     })
-    .filter(el => typeof el !== "undefined")
+    .filter(el => typeof el !== 'undefined')
   return classSelectorList
 }
 
 function printSelectorList(list, path) {
   logToFile(list, path)
 }
-
